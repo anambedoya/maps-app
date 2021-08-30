917f4bed52c82d597ac53ff74fd72978e8e4509c
Ana Maria Bedoya <anabedoya1997@gmail.com>
2021-08-30 16:38:35 -0500

    Adding fullscreen map to the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiYW5hbWJlZG95YSIsImEiOiJja3NxNndrNHIwOXprMnVxa2dwOTd0Mmc0In0.ej2k3HK1Kd3ltEiwT01v0A'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiYW5hbWJlZG95YSIsImEiOiJja3NxNndrNHIwOXprMnVxa2dwOTd0Mmc0In0.ej2k3HK1Kd3ltEiwT01v0A'
 };
 
 /*
